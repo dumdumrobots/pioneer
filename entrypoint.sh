d55b7ece2647cb0da6e1ef3a13a747c690df6f69
group1 <joaquin.cornejo@utec.edu.pe>
2024-05-06 12:36:31 +0800

    Uploading remote changes.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ source /opt/ros/humble/setup.bash
 source /home/ros/pioneer_ws/install/setup.bash
 
 sudo chmod 777 /dev/input/event*
+sudo chmod 777 /dev/ttyUBS0
 
 echo "Arguments: $@"
 
--- src/pioneer_teleop/setup.py
+++ src/pioneer_teleop/setup.py
@@ -15,8 +15,10 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name, ['package.xml']),
+
         (os.path.join('share', package_name, 'launch'), 
-         glob(os.path.join('launch', '*launch.[pxy][yma]*')))
+         glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+         
          (os.path.join('share', package_name, 'config'), 
          glob(os.path.join('config', '*config.yaml*')))
     ],
