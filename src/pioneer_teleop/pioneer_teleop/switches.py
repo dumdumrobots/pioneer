cdc1bb1220ca7bbebd3bc06edce9f4acaa101a0d
dumdumrobots <joaquin.cornejo@utec.edu.pe>
2024-05-22 19:36:44 +0800

    Latest.

--- src/pioneer_teleop/pioneer_teleop/switches.py
+++ src/pioneer_teleop/pioneer_teleop/switches.py
@@ -23,23 +23,21 @@ class Switches(Node):
         self.manual_lock = True
 
         self.dead_trigger = True
-        self.lidar_trigger = True
+        self.lidar_lock = True
 
         self.obstacle_range = 1.0472
         self.start_angle = -self.obstacle_range/2
         self.end_angle = self.obstacle_range/2
 
         self.lidar_msg = LaserScan()
-        self.lidar_msg.ranges = np.zeros(180).tolist()
-
-
-        self.get_logger().info("Current lock status \n Autonomous: {0} \n Manual: {1}".format(self.autonomous_lock,  self.manual_lock))
+        self.lidar_min_value = 0
         
         self.joy_subscriber = self.create_subscription(Joy, '/joy', self.joy_callback, 10)
         self.lidar_subscriber = self.create_subscription(LaserScan, '/scan', self.scan_callback, 10)
 
         self.man_lock_publisher = self.create_publisher(Bool, '/pause_man', 10)
         self.nav_lock_publisher = self.create_publisher(Bool, '/pause_nav', 10)
+        self.lidar_lock_publisher = self.create_publisher(Bool, '/stop_all', 10)
 
         self.interlocking_timer = self.create_timer(0.1, self.timer_callback)
 
@@ -49,24 +47,35 @@ class Switches(Node):
          # --- Lidar Switch
 
         self.data = np.array(self.lidar_msg.ranges)
+        self.angle_min = self.lidar_msg.angle_min
         self.increment = self.lidar_msg.angle_increment
+        
 
-        self.start_index = int((self.start_angle - -self.obstacle_range/2)/ self.increment)
-        self.end_index = int((self.start_angle - -self.obstacle_range/2)/ self.increment)
+        if self.increment != 0.0:
 
-        min_value = np.min(self.data[self.start_index:self.end_index])
+            self.start_index = int((self.start_angle - self.angle_min)/ self.increment)
+            self.end_index = int((self.end_angle - self.angle_min)/ self.increment)
 
-        if min_value > 0 and  min_value <= 5:
-            self.lidar_trigger  = True
+            valid_array = []
+
+            for value in self.data[self.start_index:self.end_index]:
+                if value >= 0.05:
+                    valid_array.append(value)
+
+            self.lidar_min_value = np.min(np.array(valid_array))
+
+        if self.lidar_min_value <= 0.5:
+            self.lidar_lock  = True
         else:
-            self.lidar_trigger  = False
+            self.lidar_lock  = False
 
-        self.get_logger().info("Minimal LiDAR Value: {0} ".format(min_value))
+        self.get_logger().info("Minimal LiDAR Value: {0}\n".format(self.lidar_min_value))
 
         # --- Button Switches
 
         bool_man = Bool()
         bool_nav = Bool()
+        bool_lidar = Bool()
 
         if self.joy_buttons[AXIS_TRIGGER_LEFT] == 1:
             self.dead_trigger = False
@@ -83,15 +92,17 @@ class Switches(Node):
             self.manual_lock = not self.manual_lock
 
         bool_nav.data = self.autonomous_lock or self.dead_trigger
-        bool_man.data = self.manual_lock
+        bool_man.data = self.manual_lock or self.lidar_lock
+        bool_lidar.data = self.lidar_lock
             
         self.nav_lock_publisher.publish(bool_nav)
         self.man_lock_publisher.publish(bool_man)
+        self.lidar_lock_publisher.publish(bool_lidar)
 
         self.joy_buttons_last = self.joy_buttons
 
-        self.get_logger().info("Interlocking Status: \nAutonomous: {0} \nManual: {1} \nDeadman Trigger: {2} \nLiDAR Trigger: {3} ".format(
-            self.autonomous_lock, self.manual_lock, self.dead_trigger, self.lidar_trigger))
+        self.get_logger().info("Interlocking Status:\nAutonomous: {0} Manual: {1} Deadman Trigger: {2} LiDAR Trigger: {3}\n".format(
+            self.autonomous_lock, self.manual_lock, self.dead_trigger, self.lidar_lock))
 
 
     def joy_callback(self, msg):
