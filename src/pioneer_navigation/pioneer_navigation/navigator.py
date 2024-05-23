6360d8f27b6fe8322128228e83a9bd6d71726fff
dumdumrobots <joaquin.cornejo@utec.edu.pe>
2024-05-23 10:24:24 +0800

    Latest.

--- src/pioneer_navigation/pioneer_navigation/navigator.py
+++ src/pioneer_navigation/pioneer_navigation/navigator.py
@@ -229,10 +229,12 @@ class WaypointManager(Node):
         gp1, gw1 = self.create_pose(x= 2.0, y= 0.0, w= 1.0, z= 0.0)
         self.goal_poses.append(gp1)
         self.goal_waypoints.append(gw1)
-
+        
         gp2, gw2 = self.create_pose(x= 2.0, y= 2.0, w= 0.0, z= 1.0)
         self.goal_poses.append(gp2)
         self.goal_waypoints.append(gw2)
+
+        '''
 
         gp3, gw3 = self.create_pose(x= 0.0, y= 2.0, w= 0.0, z= 1.0)
         self.goal_poses.append(gp3)
@@ -244,7 +246,8 @@ class WaypointManager(Node):
 
         gp5, gw5 = self.create_pose(x= -2.0, y= 0.0, w= 1.0, z= 0.0)
         self.goal_poses.append(gp5)
-        self.goal_waypoints.append(gw5)        
+        self.goal_waypoints.append(gw5)   
+        '''     
 
 
 def main():
