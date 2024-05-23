fe5f2468209cd0f86df0923c7b1a431bc1dcfc51
group1 <joaquin.cornejo@utec.edu.pe>
2024-05-23 18:39:29 +0800

    Latest.

--- src/pioneer_navigation/pioneer_navigation/landmarks.py
+++ src/pioneer_navigation/pioneer_navigation/landmarks.py
@@ -2,6 +2,7 @@
 
 import rclpy
 import numpy as np
+import math
 
 from geometry_msgs.msg import PoseStamped
 from nav_msgs.msg import Odometry
@@ -49,9 +50,24 @@ class LandmarkManager(Node):
 
             now = rclpy.time.Time()
             transform = self.tf_buffer.lookup_transform('map', 'odom', now)
+
+            x = transform.transform.rotation.x
+            y = transform.transform.rotation.y
+            z = transform.transform.rotation.z
+            w = transform.transform.rotation.w
+
+            roll_x, pitch_y, yaw_z = self.euler_from_quaternion(x,y,z,w)
+
+            R_iR = np.array([[np.cos(yaw_z), -np.sin(yaw_z)],
+                             [np.sin(yaw_z),  np.cos(yaw_z)]])
             
-            self.landmark_position = [transform.transform.translation.x,
-                                      transform.transform.translation.y]
+            p_R = np.array([[1],
+                            [0]])
+
+            p_i = np.dot(R_iR, p_R)
+
+            self.landmark_position = [transform.transform.translation.x + p_i[0,0],
+                                      transform.transform.translation.y + p_i[1,0]]
 
             self.landmarks.append(self.robot_pose)
 
@@ -61,3 +77,21 @@ class LandmarkManager(Node):
         msg.data = msg_array.reshape(int(msg_array.size)).tolist()
 
         self.landmark_publisher.publish(msg)
+
+
+    def euler_from_quaternion(x, y, z, w):
+
+        t0 = +2.0 * (w * x + y * z)
+        t1 = +1.0 - 2.0 * (x * x + y * y)
+        roll_x = math.atan2(t0, t1)
+     
+        t2 = +2.0 * (w * y - z * x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        pitch_y = math.asin(t2)
+     
+        t3 = +2.0 * (w * z + x * y)
+        t4 = +1.0 - 2.0 * (y * y + z * z)
+        yaw_z = math.atan2(t3, t4)
+     
+        return roll_x, pitch_y, yaw_z # in radians
