2c2546acb389ef8f6164ef4b1b14bf5772f553b4
group1 <joaquin.cornejo@utec.edu.pe>
2024-05-23 13:58:58 +0800

    working rosbags and double dockerfile implementation

--- src/pioneer_rosbags/pioneer_rosbags/pioneer_rosbags.py
+++ src/pioneer_rosbags/pioneer_rosbags/pioneer_rosbags.py
@@ -5,10 +5,8 @@ from sensor_msgs.msg import Image
 from std_msgs.msg import Bool
 
 from cv_bridge import CvBridge
-import os
 
 import cv2
-import numpy as np
 
 from datetime import datetime
 import subprocess
@@ -22,37 +20,55 @@ class Pioneer_Rosbags(Node):
         self.image_subscription = self.create_subscription(Image, '/oak/rgb/image_raw', self.image_cb, 10)
         self.stopall_subscription = self.create_subscription(Bool, '/stop_all', self.stopall_cb, 10)
         self.image_publisher = self.create_publisher(Image, '/stopall_images', 10)
+        
+        timer_period = 5
+        self.timer = self.create_timer(timer_period, self.timer_cb)
 
         self.bridge = CvBridge()
         self.stopped = False
-
         self.images = []
-        os.mkdir("/home/ros/pioneer_ws/rosbags/", 0o777)
+        #os.mkdir("/home/ros/pioneer_ws/rosbags/", 0o777)
         self.bag_process = None
+        now = datetime.now()
+        self.start_time = f"{now.year}-{now.month}-{now.day}_{now.hour}.{now.minute}.{now.second}.{now.microsecond}"
         self.start_rosbags()
 
+    def timer_cb(self):
+        if self.stopped:
+            for image in self.images:
+                self.image_publisher.publish(image[0])
+        
     def image_cb(self, msg):
         if not self.stopped:
             now = datetime.now()
-            if len(images) > 50: # 10 images / second therefore 50 images in 50 seconds
+            if len(self.images) > 50: # 10 images / second therefore 50 images in 50 seconds
                 self.images.pop(1) # Remove earliest image
-                self.images.append(msg, f"{now.year}-{now.month}-{now.day}_{now.hour}.{now.minute}.{now.second}.{now.microsecond}") # Append new images
+                self.images.append((msg, f"{now.year}-{now.month}-{now.day}_{now.hour}.{now.minute}.{now.second}.{now.microsecond}")) # Append new images
             else:
-                self.images.append(msg, f"{now.year}-{now.month}-{now.day}_{now.hour}.{now.minute}.{now.second}.{now.microsecond}")
-
+                self.images.append((msg, f"{now.year}-{now.month}-{now.day}_{now.hour}.{now.minute}.{now.second}.{now.microsecond}"))
+                
     def stopall_cb(self, msg):
         if msg.data: # Stop All True
             self.stopped = True
             # Save last 5 seconds of images
-            os.mkdir("/home/ros/pioneer_ws/stopall_images", 0o777)
+            #os.mkdir("/home/ros/pioneer_ws/stopall_images", 0o777)
             for image in self.images:
                 self.image_publisher.publish(image[0])
-                cv2_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
-                cv2.imwrite(f"/home/ros/pioneer_ws/stopall_images/{image[1]}.jpg", cv2_image)
+                cv2_image = self.bridge.imgmsg_to_cv2(image[0], desired_encoding='passthrough')
+                cv2.imwrite(f"/docker_shared/stopall_images/{image[1]}.jpg", cv2_image)
             self.stop_rosbags()
+            
+    """def stopall_test(self):
+        # Save last 5 seconds of images
+        #os.mkdir("/home/ros/pioneer_ws/stopall_images", 0o777)
+        for image in self.images:
+            self.image_publisher.publish(image[0])
+            cv2_image = self.bridge.imgmsg_to_cv2(image[0], desired_encoding='passthrough')
+            cv2.imwrite(f"/home/ros/pioneer_ws/stopall_images/{image[1]}.jpg", cv2_image)
+        self.stop_rosbags()"""
 
     def start_rosbags(self):
-        self.bag_process = subprocess.Popen(['ros2', 'bag', 'record', '/home/ros/pioneer_ws/rosbags/', '-a'])
+        self.bag_process = subprocess.Popen(['ros2', 'bag', 'record', '-o', f'/docker_shared/rosbags/{self.start_time}', '-a'])
 
     def stop_rosbags(self):
         self.bag_process.terminate()
@@ -63,7 +79,7 @@ def main():
     rclpy.init()
 
     # Create a node that subscribes to the /joy topic and publishes to the /skidbot/cmd_vel topic.
-    node = Pioneer_Image_Recognition()
+    node = Pioneer_Rosbags()
 
     # Spin the node to receive messages and call the joy_callback function for each message.
 
